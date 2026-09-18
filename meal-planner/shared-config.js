3cc13e2fdf82d6381cdd5f0678101d2cb2785e65
schottelkotte-home <mrs12642@gmail.com>
2026-09-18 19:14:19 -0400

    Connect meal planner shared backend

--- meal-planner/shared-config.js
+++ meal-planner/shared-config.js
@@ -1,1 +1,1 @@
-window.MEAL_PLANNER_SHARED_ENDPOINT = "";\n
+window.MEAL_PLANNER_SHARED_ENDPOINT = "https://script.google.com/macros/s/AKfycbypF_V3tC8WqK4WrubQJP5IXjdbGlo8G1sHHMmdRQh1NGrkJwqFTY4-1u2jS1fgNpohGw/exec";
